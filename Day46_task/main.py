5a19b2e7ad4249d985c145ee6087daeea69c90fc
Olalekan Olagbaju <olagbajuisrael11@gmail.com>
2025-09-20 22:33:59 -0230

    web_scraping

--- Day46_task/main.py
+++ Day46_task/main.py
@@ -4,4 +4,8 @@ header = {'USER-AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
 
 response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{throwback_year}/", headers=header)
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
-print(soup.findAll(id="title-of-a-story"))
+
+print(soup.title)
+# div_soup = soup.find_all("h3", id="title-of-a-story")
+# for text in div_soup:
+#     print(text.getText())
