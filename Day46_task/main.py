ff88a427b4df341ac690e4a4741bd4763f6624d6
Olalekan Olagbaju <olagbajuisrael11@gmail.com>
2025-09-22 06:18:09 -0230

    web_scraping

--- Day46_task/main.py
+++ Day46_task/main.py
@@ -1,11 +1,17 @@
 import requests, bs4
-throwback_year = input("What year music do you want to throwback to [YYYY-MM-DD]: ")
+# throwback_year = input("What year music do you want to throwback to [YYYY-MM-DD]: ")
 header = {'USER-AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36'}
 
-response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{throwback_year}/", headers=header)
+response = requests.get(url=f"https://www.billboard.com/charts/hot-100/", headers=header)
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
 
-print(soup.title)
+html_file = soup.prettify()
+
+with open ("billboard.html", "w") as file:
+    file.write(html_file)
+
+
+# print(soup.title)
 # div_soup = soup.find_all("h3", id="title-of-a-story")
 # for text in div_soup:
 #     print(text.getText())
